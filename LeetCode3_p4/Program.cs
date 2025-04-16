1170fb893ca8f74e26124b968905c3d3f20f6c91
KadinMeyer23 <vq4717wg@go.minnstate.edu>
2025-04-16 17:09:08 -0500

    Add files via upload

---
+++ LeetCode3_p4/Program.cs
@@ -0,0 +1,33 @@
+﻿//Kadin Meyer
+//4/16/2025
+
+namespace LeetCode3_p4
+{
+    public class RecentCounter
+    {
+        private Queue<int> _queue = new Queue<int>();
+        public Queue<int> _Queue
+        {
+            get { return this._queue; }
+            set { this._queue = value; }
+        }
+
+        public RecentCounter()
+        {
+            _Queue = new Queue<int>();
+
+        }
+
+
+        public int Ping(int t)
+        {
+            _Queue.Enqueue(t);
+            while (_Queue.Peek() < t - 3000)
+            {
+                _Queue.Dequeue();
+            }
+            return _Queue.Count;
+        }
+    }
+
+}
---
+++ LeetCode3_p5/Program.cs
@@ -0,0 +1,39 @@
+﻿//Kadin Meyer
+//4/16/2025
+
+namespace LeetCode3_p5
+{
+    public class Solution
+    {
+        public int CountStudents(int[] students, int[] sandwiches)
+        {
+            Queue<int> studentQueue = new Queue<int>(students);
+            Stack<int> sandwichStack = new Stack<int>();
+
+            for (int i = sandwiches.Length - 1; i >= 0; i--)
+            {
+                sandwichStack.Push(sandwiches[i]);
+            }
+
+            int count = 0;
+
+            while (studentQueue.Count > 0 && count < studentQueue.Count)
+            {
+                if (studentQueue.Peek() == sandwichStack.Peek())
+                {
+                    studentQueue.Dequeue();
+                    sandwichStack.Pop();
+                    count = 0;
+                }
+                else
+                {
+                    studentQueue.Enqueue(studentQueue.Dequeue());
+                    count++;
+                }
+            }
+
+            return studentQueue.Count;
+        }
+    }
+
+}
