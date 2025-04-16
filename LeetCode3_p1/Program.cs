c56ef8e72d1ed323f6b4d8570a5acd056dba544d
KadinMeyer23 <vq4717wg@go.minnstate.edu>
2025-04-16 17:08:39 -0500

    Add files via upload

---
+++ LeetCode3_p1/Program.cs
@@ -0,0 +1,38 @@
+﻿//Kadin Meyer
+//4/15/2025
+
+public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> Stack_Of_Chars = new Stack<char>();
+
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                Stack_Of_Chars.Push(s[i]);
+            }
+
+            if (Stack_Of_Chars.Count == 0) { return false; }
+
+            if (s[i] == ')' || s[i] == '}' || s[i] == ']')
+            {
+                char x = Stack_Of_Chars.Peek();
+                if (x == '(' && s[i] == ')') { Stack_Of_Chars.Pop(); }
+                else if (x == '{' && s[i] == '}') { Stack_Of_Chars.Pop(); }
+                else if (x == '[' && s[i] == ']') { Stack_Of_Chars.Pop(); }
+                else { return false;}
+            }
+
+
+        }
+        if (Stack_Of_Chars.Count != 0) { return false; }
+        else { return true; }
+
+
+
+
+    }
+}
---
+++ LeetCode3_p2/Program.cs
@@ -0,0 +1,38 @@
+﻿//Kadin Meyer
+//4/15/2025
+
+public class Solution
+{
+    public int CalPoints(string[] operations)
+    {
+
+        Stack<int> scores = new Stack<int>();
+
+        foreach (string operation in operations)
+        {
+            switch (operation)
+            {
+                case ("+"):
+                    {
+
+                        int lastScore = (int)scores.Pop();
+                        int newScore = lastScore + (int)scores.Peek();
+
+                        scores.Push(lastScore);
+                        scores.Push(newScore);
+                    }; break;
+                case ("D"):
+                    scores.Push((int)scores.Peek() * 2);
+                    break;
+                case ("C"):
+                    scores.Pop();
+                    break;
+                default:
+                    scores.Push((int.Parse(operation)));
+                    break;
+            }
+        }
+
+        return scores.Sum();
+    }
+}
